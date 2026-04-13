a4be81d1cfa8fb963c3c706b7f32948691ff31d2
elecbug <deveb1479@gmail.com>
2026-04-13 23:19:23 +0900

    feat: combine docker codes

--- ctl/go.mod
+++ ctl/go.mod
@@ -3,3 +3,28 @@ module github.com/elecbug/linuxus/src/ctl
 go 1.25.0
 
 require gopkg.in/yaml.v3 v3.0.1
+
+require github.com/docker/docker v28.5.2+incompatible
+
+require (
+	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/containerd/errdefs v1.0.0 // indirect
+	github.com/containerd/errdefs/pkg v0.3.0 // indirect
+	github.com/distribution/reference v0.6.0 // indirect
+	github.com/docker/go-connections v0.6.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/moby/docker-image-spec v1.3.1 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.68.0 // indirect
+	go.opentelemetry.io/otel v1.43.0 // indirect
+	go.opentelemetry.io/otel/metric v1.43.0 // indirect
+	go.opentelemetry.io/otel/trace v1.43.0 // indirect
+	golang.org/x/sys v0.42.0 // indirect
+)
--- ctl/internal/app/docker.go
+++ ctl/internal/app/docker.go
@@ -326,3 +326,148 @@ func dockerNetworkExists(name string) (bool, error) {
 	}
 	return strings.TrimSpace(out) != "", nil
 }
+
+func (a *App) buildAuthRuntimeSpec(adminSafe string) RuntimeContainerSpec {
+	networks := make([]string, 0, len(a.SafeIDs)+1)
+	for _, safeID := range a.SafeIDs {
+		networks = append(networks, a.Config.UserService.Container.NetworkPrefix+safeID)
+	}
+	networks = append(networks, a.Config.UserService.Container.NetworkPrefix+adminSafe)
+
+	return RuntimeContainerSpec{
+		Image: a.authImageName(),
+		Name:  a.Config.AuthService.Container.Name,
+		Environment: []string{
+			"TZ=" + a.Config.AuthService.Container.Timezone,
+			"AUTH_LIST=" + a.Config.AuthService.AuthListFile.ContainerPath,
+			"SESSION_SECRET=" + a.Config.AuthService.Security.SessionSecret,
+			"LOGIN_PATH=" + a.Config.AuthService.URLPath.Login,
+			"LOGOUT_PATH=" + a.Config.AuthService.URLPath.Logout,
+			"SERVICE_PATH=" + a.Config.AuthService.URLPath.Service,
+			"TERMINAL_PATH=" + a.Config.AuthService.URLPath.Terminal,
+			"USER_CONTAINER_NAME_PREFIX=" + a.Config.UserService.Container.NamePrefix,
+			"TRUSTED_PROXIES=" + a.Config.AuthService.Security.TrustedProxies,
+		},
+		Volumes: []string{
+			fmt.Sprintf("%s:%s:rw", a.Config.AuthService.AuthListFile.HostPath, a.Config.AuthService.AuthListFile.ContainerPath),
+		},
+		Ports: []string{
+			fmt.Sprintf("%d:8080", a.Config.AuthService.Container.ExternalPort),
+		},
+		Restart:  "unless-stopped",
+		Networks: networks,
+	}
+}
+
+func (a *App) buildUserRuntimeSpec(userID, safeID string) RuntimeContainerSpec {
+	return RuntimeContainerSpec{
+		Image:      a.userImageName(),
+		Name:       a.Config.UserService.Container.NamePrefix + safeID,
+		User:       fmt.Sprintf("%d:%d", a.Config.UserService.Container.Runtime.UID, a.Config.UserService.Container.Runtime.GID),
+		Hostname:   a.Config.UserService.Container.Runtime.Hostname,
+		WorkingDir: "/home/" + a.Config.UserService.Container.Runtime.User,
+		ReadOnly:   true,
+		Tmpfs: []string{
+			"/tmp:rw,noexec,nosuid,nodev,size=64m",
+			"/run:rw,noexec,nosuid,nodev,size=16m",
+			"/var/tmp:rw,noexec,nosuid,nodev,size=64m",
+		},
+		Environment: []string{
+			"TZ=" + a.Config.UserService.Container.Runtime.Timezone,
+			"CONTAINER_RUNTIME_USER=" + a.Config.UserService.Container.Runtime.User,
+			"USER_ID=" + userID,
+			"SHARED_DIR=" + a.Config.Volumes.Container.Share,
+			"READONLY_DIR=" + a.Config.Volumes.Container.Readonly,
+			"IS_ADMIN=false",
+		},
+		Volumes: []string{
+			fmt.Sprintf("%s:/home/%s:rw", a.homeDirForUser(userID), a.Config.UserService.Container.Runtime.User),
+			fmt.Sprintf("%s:%s:rw", a.Config.Volumes.Host.Share, a.Config.Volumes.Container.Share),
+			fmt.Sprintf("%s:%s:ro", a.Config.Volumes.Host.Readonly, a.Config.Volumes.Container.Readonly),
+		},
+		Restart:     "unless-stopped",
+		SecurityOpt: []string{"no-new-privileges:true"},
+		CapDrop:     []string{"ALL"},
+		Limits: ContainerLimits{
+			Memory:     a.Config.UserService.Container.User.Limits.Memory,
+			CPUs:       fmt.Sprintf("%v", a.Config.UserService.Container.User.Limits.CPU),
+			Pids:       a.Config.UserService.Container.User.Limits.PID,
+			NofileSoft: a.Config.UserService.Container.User.Limits.Ulimits.Nofile.Soft,
+			NofileHard: a.Config.UserService.Container.User.Limits.Ulimits.Nofile.Hard,
+		},
+		Networks: []string{
+			a.Config.UserService.Container.NetworkPrefix + safeID,
+		},
+	}
+}
+
+func (a *App) buildAdminRuntimeSpec(adminSafe string) RuntimeContainerSpec {
+	return RuntimeContainerSpec{
+		Image:      a.userImageName(),
+		Name:       a.Config.UserService.Container.NamePrefix + a.Config.UserService.Container.Admin.UserID,
+		User:       fmt.Sprintf("%d:%d", a.Config.UserService.Container.Runtime.UID, a.Config.UserService.Container.Runtime.GID),
+		Hostname:   a.Config.UserService.Container.Runtime.Hostname,
+		WorkingDir: "/home/" + a.Config.UserService.Container.Runtime.User,
+		ReadOnly:   true,
+		Tmpfs: []string{
+			"/tmp:rw,noexec,nosuid,nodev,size=64m",
+			"/run:rw,noexec,nosuid,nodev,size=16m",
+			"/var/tmp:rw,noexec,nosuid,nodev,size=64m",
+		},
+		Environment: []string{
+			"TZ=" + a.Config.UserService.Container.Runtime.Timezone,
+			"CONTAINER_RUNTIME_USER=" + a.Config.UserService.Container.Runtime.User,
+			"USER_ID=" + a.Config.UserService.Container.Admin.UserID,
+			"SHARED_DIR=" + a.Config.Volumes.Container.Share,
+			"READONLY_DIR=" + a.Config.Volumes.Container.Readonly,
+			"IS_ADMIN=true",
+		},
+		Volumes: []string{
+			fmt.Sprintf("%s:/home/%s:rw", a.homeDirForUser(a.Config.UserService.Container.Admin.UserID), a.Config.UserService.Container.Runtime.User),
+			fmt.Sprintf("%s:%s:rw", a.Config.Volumes.Host.Share, a.Config.Volumes.Container.Share),
+			fmt.Sprintf("%s:%s:rw", a.Config.Volumes.Host.Readonly, a.Config.Volumes.Container.Readonly),
+		},
+		Restart:     "unless-stopped",
+		SecurityOpt: []string{"no-new-privileges:true"},
+		CapDrop:     []string{"ALL"},
+		Limits: ContainerLimits{
+			Memory:     a.Config.UserService.Container.Admin.Limits.Memory,
+			CPUs:       fmt.Sprintf("%v", a.Config.UserService.Container.Admin.Limits.CPU),
+			Pids:       a.Config.UserService.Container.Admin.Limits.PID,
+			NofileSoft: a.Config.UserService.Container.Admin.Limits.Ulimits.Nofile.Soft,
+			NofileHard: a.Config.UserService.Container.Admin.Limits.Ulimits.Nofile.Hard,
+		},
+		Networks: []string{
+			a.Config.UserService.Container.NetworkPrefix + adminSafe,
+		},
+	}
+}
+
+func (a *App) buildRuntimeNetworks() ([]RuntimeNetworkSpec, error) {
+	networks := make([]RuntimeNetworkSpec, 0, len(a.SafeIDs)+1)
+	seq := 0
+
+	for _, safeID := range a.SafeIDs {
+		subnet, err := getIP(a.Config.UserService.Container.BaseIP, seq)
+		if err != nil {
+			return nil, err
+		}
+		networks = append(networks, RuntimeNetworkSpec{
+			Name:   a.Config.UserService.Container.NetworkPrefix + safeID,
+			Subnet: subnet,
+		})
+		seq++
+	}
+
+	adminSafe := sanitizeName(a.Config.UserService.Container.Admin.UserID)
+	subnet, err := getIP(a.Config.UserService.Container.BaseIP, seq)
+	if err != nil {
+		return nil, err
+	}
+	networks = append(networks, RuntimeNetworkSpec{
+		Name:   a.Config.UserService.Container.NetworkPrefix + adminSafe,
+		Subnet: subnet,
+	})
+
+	return networks, nil
+}
