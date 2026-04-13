08db6fc9ab8ef9b069c17b8a8356e273e244bf92
elecbug <deveb1479@gmail.com>
2026-04-14 00:02:59 +0900

    feat: update docker api more 3

--- ctl/internal/app/docker.go
+++ ctl/internal/app/docker.go
@@ -1,16 +1,22 @@
 package app
 
 import (
+	"archive/tar"
+	"bytes"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"strconv"
 	"strings"
 
+	"github.com/docker/docker/api/types/build"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/api/types/network"
+	"github.com/docker/docker/pkg/jsonmessage"
 	"github.com/docker/go-connections/nat"
+	"github.com/moby/term"
 )
 
 func (a *App) GenerateService() error {
@@ -141,15 +147,108 @@ func (a *App) VolumeClean() error {
 
 func (a *App) buildRuntimeImages() error {
 	fmt.Println("[+] Building runtime images...")
-	if err := runCmd("sudo", "docker", "build", "-t", a.authImageName(), a.Config.AuthService.SourceDir); err != nil {
+
+	if a.DockerClient == nil {
+		return fmt.Errorf("Docker client is not initialized")
+	}
+
+	if err := a.buildImage(a.Config.AuthService.SourceDir, a.authImageName(), nil); err != nil {
+		return fmt.Errorf("failed to build auth image: %w", err)
+	}
+
+	if err := a.buildImage(a.Config.UserService.SourceDir, a.userImageName(), map[string]*string{
+		"CONTAINER_RUNTIME_USER": strPtr(a.Config.UserService.Container.Runtime.User),
+	}); err != nil {
+		return fmt.Errorf("failed to build user image: %w", err)
+	}
+
+	return nil
+}
+
+func (a *App) buildImage(sourceDir string, tag string, buildArgs map[string]*string) error {
+	buildCtx, err := tarBuildContext(sourceDir)
+	if err != nil {
 		return err
 	}
-	return runCmd(
-		"sudo", "docker", "build",
-		"--build-arg", "CONTAINER_RUNTIME_USER="+a.Config.UserService.Container.Runtime.User,
-		"-t", a.userImageName(),
-		a.Config.UserService.SourceDir,
+
+	resp, err := a.DockerClient.ImageBuild(a.Context, buildCtx, build.ImageBuildOptions{
+		Tags:       []string{tag},
+		Dockerfile: "Dockerfile",
+		Remove:     true,
+		BuildArgs:  buildArgs,
+	})
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	fd, isTerm := term.GetFdInfo(os.Stdout)
+
+	err = jsonmessage.DisplayJSONMessagesStream(
+		resp.Body,
+		os.Stdout,
+		fd,
+		isTerm,
+		nil,
 	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func tarBuildContext(dir string) (io.Reader, error) {
+	buf := new(bytes.Buffer)
+	tw := tar.NewWriter(buf)
+
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, walkErr error) error {
+		if walkErr != nil {
+			return walkErr
+		}
+
+		relPath, err := filepath.Rel(dir, path)
+		if err != nil {
+			return err
+		}
+		if relPath == "." {
+			return nil
+		}
+
+		header, err := tar.FileInfoHeader(info, "")
+		if err != nil {
+			return err
+		}
+		header.Name = filepath.ToSlash(relPath)
+
+		if err := tw.WriteHeader(header); err != nil {
+			return err
+		}
+
+		if info.Mode().IsRegular() {
+			f, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+
+			if _, err := io.Copy(tw, f); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
+	if err != nil {
+		_ = tw.Close()
+		return nil, err
+	}
+
+	if err := tw.Close(); err != nil {
+		return nil, err
+	}
+
+	return buf, nil
 }
 
 func (a *App) ensureRuntimeNetworks() error {
