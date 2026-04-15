2b9b482e9632d51e493ac29db6fe4420d588d98d
elecbug <deveb1479@gmail.com>
2026-04-15 21:06:13 +0900

    feat: add manager dir

---
+++ src/manager/cmd/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Linuxus Manager!")
+
+	for {
+	}
+}
---
+++ src/manager/go.mod
@@ -0,0 +1,4 @@
+module github.com/elecbug/linuxus/src/manager
+
+go 1.25.0
+
